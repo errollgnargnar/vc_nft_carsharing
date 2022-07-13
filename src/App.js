07805ad5fe50b6d20608a5b9776a9bf057437a0e
errollgnargnar <errollgnargnar@gmail.com>
2022-07-13 18:42:15 -0500

    frontend heroku

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import { Outlet } from "react-router-dom";
+import { Outlet,  Routes,
+  Route, } from "react-router-dom";
 
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container';
 import Nav from "react-bootstrap/Nav";
 import Navbar from 'react-bootstrap/Navbar'
 
+import About from './routes/About';
+import Car from './routes/Car';
+import Cars from './routes/Cars';
+
 import './App.css';
 
 function App({connectToWallet}) {
   
+  return (
+    <div>
+      {/* Routes nest inside one another. Nested route paths build upon
+            parent route paths, and nested route elements render inside
+            parent route elements. See the note about <Outlet> below. */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<About />} />
+          <Route path="cars" element={<Cars />} >
+              <Route
+              index
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p style={{textAlign:"center"}}>Select a Vehicle</p>
+                </main>
+              }
+            />
+            <Route path=":carId" element={<Car />} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+
+        </Route> 
+      </Routes>
+    </div>
+  );
+}
+
+function Layout() {
   return (
     <div className="App">
       <div className="navheader" style={{display:"flex", justifyContent:"space-around", alignItems:"center",   borderBottom: "1px solid"}}>
--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
   BrowserRouter,
-  Routes,
-  Route,
+
 } from "react-router-dom";
 import './index.css';
 import App from './App';
 
-import About from './routes/About';
-import Car from './routes/Car';
-import Cars from './routes/Cars';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -19,31 +15,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<About />} />
-        <Route path="cars" element={<Cars />} >
-            <Route
-            index
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p style={{textAlign:"center"}}>Select a Vehicle</p>
-              </main>
-            }
-          />
-          <Route path=":carId" element={<Car />} />
-        </Route>
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>There's nothing here!</p>
-            </main>
-          }
-        />
-
-      </Route> 
-
-    </Routes>
+    <App />
   </BrowserRouter>
 );
